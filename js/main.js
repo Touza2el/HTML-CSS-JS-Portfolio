e3634d332ba120505378888981beed717a6c6b53
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-08-15 15:02:33 +0200

    Second Modification

--- js/main.js
+++ js/main.js
@@ -39,11 +39,13 @@ window.onscroll = function () {
 // Start Header Section
 
 const headerSection = document.querySelector('.header-section');
+const headerLinksContainer = document.querySelector('.header-links');
 const headerLinks = document.querySelectorAll('.header-links li a');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 // Toggle Between Header Links
 headerLinks.forEach((link) => {
-  link.addEventListener('click', function () {
+  link.addEventListener('click', function (event) {
     headerLinks.forEach((link) => {
       link.classList.remove('active');
     });
@@ -51,4 +53,46 @@ headerLinks.forEach((link) => {
   });
 });
 
+// Hide / Show Mobile Menu
+mobileMenu.addEventListener('click', function () {
+  headerLinksContainer.classList.toggle('active');
+});
+
 // End Header Section
+
+// Start Gallery Section
+
+const galleryButtons = document.querySelectorAll('.gallery-item button');
+let galleryImages = document.querySelectorAll('.gallery-item img');
+galleryImages = Array.from(galleryImages);
+const popupBoxContainer = document.querySelector('.popup-box-container');
+let popupBoxImage = document.querySelector('.popup-box-image');
+const closePopupBox = document.querySelector('.close-popup-box');
+
+galleryButtons.forEach((button) => {
+  button.addEventListener('click', function (event) {
+    const btnDataset = event.currentTarget.dataset.btn;
+    const currentImage = galleryImages.find((image) => {
+      return image.dataset.image === btnDataset;
+    });
+    const currentImageSource = currentImage.src;
+    const currentImageAlt = currentImage.alt;
+
+    const imageTitle = document.createElement('h3');
+    const imageTitleText = document.createTextNode(currentImageAlt);
+    imageTitle.appendChild(imageTitleText);
+    popupBoxImage.appendChild(imageTitle);
+
+    const imageElement = document.createElement('img');
+    imageElement.src = currentImageSource;
+    popupBoxImage.appendChild(imageElement);
+    popupBoxContainer.style.display = 'flex';
+    console.log(popupBoxContainer);
+  });
+});
+
+closePopupBox.addEventListener('click', function () {
+  popupBoxContainer.style.display = 'none';
+  popupBoxImage.innerHTML = null;
+});
+// End Gallery Section
